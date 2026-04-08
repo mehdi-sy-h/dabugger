1d3d46cbafa4ee69ae87def3f9c6b58d1881357c
Mehdi <mehdi.h.business@pm.me>
2026-04-08 06:47:53 +0100

    Format description skeleton

--- src/dwarf.c
+++ src/dwarf.c
@@ -4,13 +4,19 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct Cursor {
 	const uint8_t *ptr;
 	size_t remaining;
 } Cursor;
+
+/* TODO: Implement and replace naked advancements with this to incorporate
+ * safety checks. */
+static inline void advance_cursor(Cursor *cursor, size_t n) {};
 
 static inline void read_bytes(Cursor *cursor, void *dst, size_t n) {
 	n = n <= cursor->remaining ? n : cursor->remaining;
@@ -19,7 +25,7 @@ static inline void read_bytes(Cursor *cursor, void *dst, size_t n) {
 	cursor->remaining -= n;
 }
 
-static LineNumProgHeader64 parse_line_header64(Cursor *cursor) {
+static LineNumProgHeader64 alloc_line_header64(Cursor *cursor) {
 	LineNumProgHeader64 header;
 
 	read_bytes(cursor, &header.unit_length, sizeof(header.unit_length));
@@ -50,17 +56,112 @@ static LineNumProgHeader64 parse_line_header64(Cursor *cursor) {
 	 * types, need to consider ownership as well. Directories and file names
 	 * will most likely need to be stored on the heap because of the variation
 	 * in format types. */
+
+	header.directory_entry_format =
+		malloc(sizeof(DwarfFormatDesc) * header.directory_entry_format_count);
+
+	/* TODO: Safety checks */
 	for (uint8_t i = 0; i < header.directory_entry_format_count; i++) {
 		ULEBReadResult content_type = read_uleb128(cursor->ptr);
 		cursor->ptr += content_type.bytes_read;
 		cursor->remaining -= content_type.bytes_read;
 
+		header.directory_entry_format[i].content_type = content_type.value;
+
 		ULEBReadResult form_code = read_uleb128(cursor->ptr);
 		cursor->ptr += form_code.bytes_read;
 		cursor->remaining -= form_code.bytes_read;
+
+		header.directory_entry_format[i].form_code = form_code.value;
+	}
+
+	ULEBReadResult directories_count = read_uleb128(cursor->ptr);
+	cursor->ptr += directories_count.bytes_read;
+	cursor->remaining -= directories_count.bytes_read;
+
+	header.directories_count = directories_count.value;
+
+	/* TODO: directories */
+
+	for (uint64_t i = 0; i < header.directories_count; i++) {
+		for (uint8_t j = 0; j < header.directory_entry_format_count; j++) {
+			DwarfFormatDesc format_desc = header.directory_entry_format[j];
+			switch (format_desc.content_type) {
+			case DW_LNCT_path:
+				if (format_desc.form_code == DW_FORM_string) {
+					/* TODO: Read null terminated string */
+				} else if (format_desc.form_code == DW_FORM_line_strp ||
+						   format_desc.form_code == DW_FORM_strp ||
+						   format_desc.form_code == DW_FORM_strp_sup) {
+					/* TODO: Read uint8_t offset */
+				} else if (format_desc.form_code == DW_FORM_strx ||
+						   format_desc.form_code == DW_FORM_strx1 ||
+						   format_desc.form_code == DW_FORM_strx2 ||
+						   format_desc.form_code == DW_FORM_strx3 ||
+						   format_desc.form_code == DW_FORM_strx4) {
+					/* TODO: This is .debug_line.dwo section stuff idk if I'll
+					 * handle it (or split objects at all) */
+				} else {
+					/* TODO: Handle invalid case */
+				}
+				break;
+			case DW_LNCT_directory_index:
+				if (format_desc.form_code == DW_FORM_data1) {
+					/* TODO: Read uint8_t */
+				} else if (format_desc.form_code == DW_FORM_data2) {
+					/* TODO: Read uint16_t */
+				} else if (format_desc.form_code == DW_FORM_udata) {
+					/* TODO: Read ULEB128 */
+				} else {
+					/* TODO: Handle invalid case */
+				}
+				break;
+			case DW_LNCT_timestamp:
+				if (format_desc.form_code == DW_FORM_udata) {
+					/* TODO: Read ULEB128 */
+				} else if (format_desc.form_code == DW_FORM_data4) {
+					/* TODO: Read uint32_t */
+				} else if (format_desc.form_code == DW_FORM_data8) {
+					/* TODO: Read uint64_t */
+				} else if (format_desc.form_code == DW_FORM_block) {
+					/* TODO: Read ULEB128 length n followed by n bytes */
+				} else {
+					/* TODO: Handle invalid case */
+				}
+				break;
+			case DW_LNCT_size:
+				if (format_desc.form_code == DW_FORM_udata) {
+					/* TODO: Read ULEB128 */
+				} else if (format_desc.form_code == DW_FORM_data1) {
+					/* TODO: Read uint8_t */
+				} else if (format_desc.form_code == DW_FORM_data2) {
+					/* TODO: Read uint16_t */
+				} else if (format_desc.form_code == DW_FORM_data4) {
+					/* TODO: Read uint32_t */
+				} else if (format_desc.form_code == DW_FORM_data8) {
+					/* TODO: Read uint64_t */
+				} else {
+					/* TODO: Handle invalid case */
+				}
+				break;
+			case DW_LNCT_MD5:
+				if (format_desc.form_code == DW_FORM_data16) {
+					/* TODO: Read 16 byte MD5 digest */
+				} else {
+					/* TODO: Handle invalid case */
+				}
+			default:
+				/* TODO: Handle invalid case */;
+			}
+		}
 	}
 
 	return header;
+}
+
+static void free_line_header64(LineNumProgHeader64 *header) {
+	free(header->directory_entry_format);
+	free(header->file_name_entry_format);
 }
 
 void parse_debug_line_section(DebugLineSection debug_line_section) {
@@ -82,5 +183,7 @@ void parse_debug_line_section(DebugLineSection debug_line_section) {
 
 	Cursor cursor = {.ptr = debug_line_section.data,
 					 .remaining = debug_line_section.size};
-	parse_line_header64(&cursor);
+
+	LineNumProgHeader64 header = alloc_line_header64(&cursor);
+	free_line_header64(&header);
 }
--- src/dwarf.h
+++ src/dwarf.h
@@ -23,13 +23,61 @@ typedef struct LineNumStateMachine {
 	bool epilogue_begin;
 } LineNumStateMachine;
 
+/* TODO: Fix these defs */
 typedef uint8_t InitialLength32;
 
 typedef struct InitialLength64 {
 	uint8_t value[12];
 } InitialLength64;
 
-typedef uint64_t ULEB128Value;
+/* See Dwarf 5 Specification 7.22 */
+typedef enum DwarfLineNumContentType {
+	DW_LNCT_path = 0x1,
+	DW_LNCT_directory_index = 0x2,
+	DW_LNCT_timestamp = 0x3,
+	DW_LNCT_size = 0x4,
+	DW_LNCT_MD5 = 0x5,
+	DW_LNCT_lo_user = 0x2000,
+	DW_LNCT_hi_user = 0x3fff,
+} DwarfLineNumContentType;
+
+/* See Dwarf 5 Specification 7.5.5 */
+typedef enum DwarfFormCode {
+	DW_FORM_data1      = 0x0b,
+	DW_FORM_data2      = 0x05,
+	DW_FORM_data4      = 0x06,
+	DW_FORM_data8      = 0x07,
+	DW_FORM_data16     = 0x1e,
+	DW_FORM_sdata      = 0x0d,
+	DW_FORM_udata      = 0x0f,
+
+	DW_FORM_string     = 0x08,
+	DW_FORM_strp       = 0x0e,
+	DW_FORM_line_strp  = 0x1f,
+	DW_FORM_strp_sup   = 0x1d,
+
+	DW_FORM_strx       = 0x1a,
+	DW_FORM_strx1      = 0x25,
+	DW_FORM_strx2      = 0x26,
+	DW_FORM_strx3      = 0x27,
+	DW_FORM_strx4      = 0x28,
+
+	DW_FORM_block      = 0x09,
+	DW_FORM_block1     = 0x0a,
+	DW_FORM_block2     = 0x03,
+	DW_FORM_block4     = 0x04,
+
+	DW_FORM_flag       = 0x0c,
+
+	DW_FORM_sec_offset = 0x17,
+} DwarfFormCode;
+
+/* TODO: Enums to specify what the possible content type and form code combinations are,
+ * maybe tagged union. */
+typedef struct DwarfFormatDesc {
+	DwarfLineNumContentType content_type;
+	DwarfFormCode form_code;
+} DwarfFormatDesc;
 
 /* TODO: Type for 32 bit dwarf header */
 
@@ -39,7 +87,7 @@ typedef struct LineNumProgHeader64 {
 	uint16_t version;
 	uint8_t address_size;
 	uint8_t segment_selector_size;
-	size_t header_length;
+	size_t header_length; /* TODO: This is dependent on the object file not the system */
 	uint8_t minimum_instruction_length;
 	uint8_t maximum_operations_per_instruction;
 	uint8_t default_is_stmt;
@@ -48,12 +96,12 @@ typedef struct LineNumProgHeader64 {
 	uint8_t opcode_base;
 	const uint8_t *standard_opcode_lengths;
 	uint8_t directory_entry_format_count;
-	ULEB128Value *directory_entry_format[2];
-	ULEB128Value directories_count;
+	DwarfFormatDesc *directory_entry_format;
+	uint64_t directories_count;
 	/* TODO: directories */
 	uint8_t file_name_entry_format_count;
-	ULEB128Value *file_name_entry_format[2];
-	ULEB128Value file_names_count;
+	DwarfFormatDesc *file_name_entry_format;
+	uint64_t file_names_count;
 	/* TODO: file_names */
 } LineNumProgHeader64;
 
