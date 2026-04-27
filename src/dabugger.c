26eb70970b0f36b1ca7f85269be55bf0875cac59
Mehdi <mehdi.h.business@pm.me>
2026-04-27 16:54:30 +0100

    Debugger setup

--- src/dabugger.c
+++ src/dabugger.c
@@ -5,13 +5,17 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/personality.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, [[maybe_unused]] char *argv[argc + 1]) {
 	if (argc < 2) {
-		// TODO: Attach mode
+		/* TODO: Attach mode
+		 * This will require handling PIE/ASLR. */
 		printf("Attach mode not implemented yet");
 		return EXIT_SUCCESS;
 	}
@@ -20,11 +24,16 @@ int main(int argc, [[maybe_unused]] char *argv[argc + 1]) {
 	const char *inferior_path = argv[1];
 
 	if (pid == 0) {
-		// Child process
-		// execl(inferior_path, argv);
-		// ptrace(PTRACE_TRACEME);
+		/* Child process */
+		ptrace(PTRACE_TRACEME);
+		personality(ADDR_NO_RANDOMIZE);
+		execl(inferior_path, *argv);
 	} else if (pid > 0) {
-		// Parent process
+		/* Parent process */
+		int status;
+		waitpid(pid, &status, __WALL);
+		ptrace(PTRACE_SETOPTIONS, pid, NULL, PTRACE_O_EXITKILL);
+
 		DebugSections debug_sections = parse_elf64_file(argv[1]);
 		parse_debug_line_section(debug_sections);
 
